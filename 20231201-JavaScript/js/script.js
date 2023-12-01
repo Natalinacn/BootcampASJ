c7216d73605874197b3f673b241c9a5ce0faf701
Natalinacn <natalinacuna@gmail.com>
2023-12-01 13:01:53 -0300

    Subiendo ejercicios de JavaScript

---
+++ 20231201-JavaScript/js/script.js
@@ -0,0 +1,99 @@
+//Ejercicio 1: Cantidad de caracteres
+
+let texto = prompt("Ingrese un texto breve");
+
+let cantCarac = texto.length;
+
+console.log("El texto tiene " + cantCarac + " caracteres");
+
+
+// Ejercicio 2: Calculadora de edad
+
+// let edad = prompt("Ingrese su edad: ");
+
+// let cantDias = edad * 365;
+
+// console.log("El usuario tiene " + cantDias + " días de nacido");
+
+
+
+//Ejercicio 3: Suma De Valores
+
+
+
+// let num1 = parseInt(prompt("Ingrese el número 1"));
+// let num2= parseInt(prompt("Ingrese el número 2"));
+
+// let resultado = num1 + num2;
+
+// console.log("El resultado de la suma es de: " + resultado);
+
+
+
+//Ejercicio 4: Calculador de abastecimiento de por vida
+
+//Ejercicio 1: ¿Cuántos snacks vas a comer por el resto de tu vida? 
+
+// let edad = parseInt(prompt("Ingrese su edad"));
+
+// let maxEdad = parseInt(prompt("Ingrese su edad máxima"));
+
+// let snackFav = prompt("Ingrese su snack favorito");
+
+// let snackPorDia = parseInt(prompt("¿Cuantos snacks comerás por dia?"));
+
+// let precioSnack = parseInt(prompt("Ingresá el precio por unidad de cada snack"));
+
+
+// let cantDias = (maxEdad - edad) * 365; 
+
+// let cantSnack = cantDias * snackPorDia;
+
+// let totalSnack = precioSnack * cantDias;
+
+// alert("Necesitarás " + cantSnack + " snacks para que te alcancen hasta los " + maxEdad + " XX años.");
+// alert("Vas a gastar " + totalSnack + " pesos en snacks");
+
+
+//Ejercicio 2: Vacaciones
+
+// let cantDiasViaje = parseInt(prompt("Ingrese la cantidad de días que va a estar de viaje"));
+
+// let presupuesto = parseInt(prompt("Ingrese su presupuesto máximo por todo el viaje"));
+
+// let comidaPorDia = parseInt(prompt("Ingrese cuántas comidas por día vas a tener en todo tu viaje"));
+
+// let presupuestoComida = (presupuesto / cantDiasViaje) / comidaPorDia;
+
+
+// alert("Podés gastar " + presupuestoComida + " en cada comida para que te alcance la plata durante los " + cantDiasViaje + " días de viaje");
+
+
+
+//Ejercicios: ES6
+
+//Ejercicio 1
+
+// let nombre = "Gabriela";
+// let profesion = "programadora";
+
+
+// let saludo = `${nombre} es ${profesion}`;
+
+// console.log(saludo);
+
+
+
+//Ejercicio 2
+
+// let precioArt = parseInt(prompt("Ingresá el precio del artículo"));
+
+// let cantArt = parseInt(prompt("Ingresá la cantidad de artículos que llevarás"));
+
+// console.log(`El total a abonar es de ${precioArt * cantArt} pesos`);
+
+
+
+
+
+
