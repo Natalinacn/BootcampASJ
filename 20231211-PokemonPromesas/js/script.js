af5aaf902d07853a42e7b01d5d873150310dc177
Natalin <99100482+Natalinacn@users.noreply.github.com>
2023-12-13 08:42:47 -0300

    Update script.js

--- 20231211-PokemonPromesas/js/script.js
+++ 20231211-PokemonPromesas/js/script.js
@@ -17,25 +17,4 @@ const chisteElement = document.getElementById('chiste');
 
         actualizar.addEventListener("click", obtenerChiste);
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const chisteElement = document.getElementById('chiste');
-//     const actualizarBtn = document.getElementById('actualizarBtn');
-
-//     // Función para obtener un chiste de Chuck Norris
-//     function obtenerChiste() {
-//         fetch('https://api.chucknorris.io/jokes/random')
-//             .then(response => response.json())
-//             .then(data => {
-//                 // Inserta el chiste en el elemento h2
-//                 chisteElement.textContent = data.value;
-//             })
-//             .catch(error => console.error('Error al obtener chiste:', error));
-//     }
-
-//     // Llama a obtenerChiste al cargar la página
-//     obtenerChiste();
-
-//     // Agrega un evento click al botón para actualizar el chiste
-//     actualizarBtn.addEventListener('click', obtenerChiste);
-// });
 
